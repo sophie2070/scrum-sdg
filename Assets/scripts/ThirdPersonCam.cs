8ac1b4c226c190be26086c97779cc0f5791f6893
fiplezer <jessebraaksma12@gmail.com>
2025-09-18 11:51:56 +0200

    cam&player movement done

--- Assets/scripts/ThirdPersonCam.cs
+++ Assets/scripts/ThirdPersonCam.cs
@@ -21,11 +21,9 @@ public class ThirdPersonCam : MonoBehaviour
 
     private void Update()
     {
-        // rotatie van orientation
-        Vector3 viewDir = player.position - new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
 
-        // rotatie playerobj
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
